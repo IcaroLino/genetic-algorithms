b0edb3e66b2ec3a921dac616da7af5b356a3b8b7
Icaro <luizicaro.s@gmail.com>
2023-12-03 15:42:14 -0300

    Implement index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
+import mutationType from './enum/mutationType';
+import Group from './models/Group';
 import Individual from './models/Individual';
 import objectiveFn from './objective/ObjectiveFn';
+import * as fs from 'fs';
 
-const objFn = {objectiveFunction: objectiveFn };
-const min = [0, 0];
-const max = [31, 31];
-const decimalPrecision = 3;
+const objFn = { objectiveFunction: objectiveFn };
+const min = [-500, 500];
+const max = [500, 500];
+const decimalPrecision = 2;
+const mttType = mutationType.perINDIVIDUAL;
+const mutationRate = 5;
 
-Individual.setGroupParams(objFn, min, max, decimalPrecision);
-const a = new Individual(2, 5);
-const b = new Individual(2, 7);
-const c = new Individual(2, 9);
 
-console.log('a:', a);
-console.log('b:', b);
-console.log('c:', c);
+Individual.setGroupParams(objFn, min, max, decimalPrecision, mttType, mutationRate);
+
+const groupLength = 30;
+const dimensions = min.length;
+const individualLength = 18;
+const generations = 60;
+
+const a = new Group(groupLength, dimensions, individualLength, generations);
+a.printMetricsHistory(decimalPrecision);
+
+try {
+    fs.writeFile('group.txt', a.getGroupLog, (err) => {
+        if (err) throw new Error('File writing Erro:', err);
+    });
+} catch (e) {
+    console.error((e as Error).message);
+}
